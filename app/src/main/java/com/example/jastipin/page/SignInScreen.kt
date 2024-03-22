13ec7197d7c657f2f69d8989a06c382affe2d843
Royhan <royhankeren12@gmail.com>
2024-03-22 23:02:21 +0700

    signInScreen update

--- app/src/main/java/com/example/jastipin/page/SignInScreen.kt
+++ app/src/main/java/com/example/jastipin/page/SignInScreen.kt
@@ -1,32 +1,58 @@
 package com.example.jastipin.page
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.IconButton
 import androidx.compose.material.Surface
 import androidx.compose.material.TabRow
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,11 +60,14 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.jastipin.R
+import com.example.jastipin.ui.theme.black
 import com.example.jastipin.ui.theme.grey
+import com.example.jastipin.ui.theme.lightOrange
+import com.example.jastipin.ui.theme.lightSkin
 import com.example.jastipin.ui.theme.orange
 
 @Composable
-fun SignInScreen(navController: NavController) {
+fun HalamanSignInScreen(navController: NavController) {
     var selectTabIndex by remember {
         mutableStateOf(0)
     }
@@ -53,7 +82,7 @@ fun SignInScreen(navController: NavController) {
         ) {
             Image(
                 painter = painterResource(id = R.drawable.umkm1),
-                contentDescription = "umkm-1",
+                contentDescription = stringResource(id = R.string.umkm1),
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(229.dp)
@@ -119,8 +148,234 @@ fun SignInScreen(navController: NavController) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignInScreen(navController: NavController) {
+    var txtEmail by rememberSaveable {
+        mutableStateOf("")
+    }
+    var txtPassword by rememberSaveable {
+        mutableStateOf("")
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    var obsucureText by remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
+        OutlinedTextField(
+            value = txtEmail,
+            onValueChange = { txtEmail = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp),
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.email),
+                    style = TextStyle(
+                        color = Color.Gray,
+                        fontSize = 15.sp
+                    ),
+                )
+            },
+            maxLines = 1,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    keyboardController?.hide()
+                }
+            ),
+            textStyle = TextStyle(
+                fontSize = 14.sp,
+                color = Color.Black,
+            ),
+            shape = RoundedCornerShape(
+                10.dp
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = txtPassword,
+            onValueChange = { txtPassword = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp),
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.kata_sandi),
+                    style = TextStyle(
+                        color = Color.Gray,
+                        fontSize = 15.sp
+                    ),
+                )
+            },
+            maxLines = 1,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    keyboardController?.hide()
+                }
+            ),
+            textStyle = TextStyle(
+                fontSize = 14.sp,
+                color = Color.Black,
+            ),
+            shape = RoundedCornerShape(
+                10.dp
+            ),
+            visualTransformation = if (obsucureText)
+                PasswordVisualTransformation()
+            else VisualTransformation.None,
+            trailingIcon = {
+                IconButton(onClick = {
+                    obsucureText = !obsucureText
+                }) {
+                    val visibilityIcon = if (obsucureText)
+                        Icons.Filled.VisibilityOff
+                    else Icons.Filled.Visibility
+
+                    val description = if (obsucureText)
+                        "Hide Password"
+                    else "Show Password"
+
+                    Icon(
+                        imageVector = visibilityIcon,
+                        contentDescription = description
+                    )
+                }
+            }
+        )
+        TextButton(
+            onClick = {/*TODO NOTHING HERE*/ },
+            modifier = Modifier.align(
+                Alignment.End
+            )
+        ) {
+            Text(
+                text = stringResource(id = R.string.lupa_password),
+                style = TextStyle(
+                    color = black,
+                    fontSize = 14.sp
+                )
+            )
+        }
+        Button(
+            onClick = {
+                navController.popBackStack(
+                    // TODO THIS NOT FUNCTION
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(45.dp)
+                .align(Alignment.CenterHorizontally),
+            colors = ButtonDefaults.buttonColors(
+                orange
+            ),
+            shape = RoundedCornerShape(
+                size = 20.dp
+            )
+        ) {
+            Text(
+                text = stringResource(id = R.string.masuk),
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 15.sp
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            Alignment.CenterVertically
+        ) {
+            Divider(
+                modifier = Modifier
+                    .width(150.dp)
+                    .clip(
+                        shape = RoundedCornerShape(
+                            size = 20.dp
+                        )
+                    ),
+                color = lightOrange,
+                thickness = 2.dp
+            )
+            Text(
+                text = stringResource(id = R.string.atau),
+                fontSize = 15.sp,
+                color = grey,
+            )
+            Divider(
+                modifier = Modifier
+                    .width(150.dp)
+                    .clip(
+                        shape = RoundedCornerShape(
+                            size = 20.dp
+                        )
+                    ),
+                color = lightOrange,
+                thickness = 2.dp
+
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Surface(
+                modifier = Modifier
+                    .width(60.dp)
+                    .height(60.dp)
+                    .clip(
+                        shape = RoundedCornerShape(
+                            size = 8.dp
+                        )
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = lightOrange,
+                        shape = RoundedCornerShape(
+                            size = 8.dp
+                        )
+                    ),
+                color = Color.White
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.google),
+                    contentDescription = stringResource(
+                        id = R.string.google
+                    ),
+                    modifier = Modifier.size(30.dp)
+                        .scale(0.5f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
+}
+
 @Preview(showSystemUi = true)
 @Composable
 fun PreviewSignInScreen() {
     SignInScreen(navController = rememberNavController())
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewHalamanSignInScreen() {
+    HalamanSignInScreen(navController = rememberNavController())
 }
--- app/src/main/java/com/example/jastipin/ui/theme/Color.kt
+++ app/src/main/java/com/example/jastipin/ui/theme/Color.kt
@@ -10,5 +10,24 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val orange = Color(0xFFFE3F19)
+val orange = Color(0xFFFA6541)
 val grey = Color(0xFFBABABA)
+val black = Color(0xFF1F2A37)
+val lightSkin = Color(0xFFFFE8E3)
+val lightGrey = Color(0xFFD9D9D9)
+
+const val originalColor = 0xFFFA6541
+
+const val originalRed = (originalColor shr 16) and 0xFF
+const val originalGreen = (originalColor shr 8) and 0xFF
+const val originalBlue = originalColor and 0xFF
+
+const val increasePercentage = 0.5 // Misalnya, 50% peningkatan untuk membuat lebih putih
+val newRed = (originalRed + (255 * increasePercentage)).coerceAtMost(255.0).toInt()
+val newGreen = (originalGreen + (255 * increasePercentage)).coerceAtMost(255.0).toInt()
+val newBlue = (originalBlue + (255 * increasePercentage)).coerceAtMost(255.0).toInt()
+
+val newColor = (newRed shl 16) or (newGreen shl 8) or newBlue
+val newColorWithAlpha = newColor or 0xFF000000.toInt()
+
+val lightOrange = Color(newColorWithAlpha)
