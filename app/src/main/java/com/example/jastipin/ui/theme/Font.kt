c929246b0bbd1994a1cee455ad5070472fd23e12
Royhan <royhankeren12@gmail.com>
2024-03-25 12:29:27 +0700

    add Font

---
+++ app/src/main/java/com/example/jastipin/ui/theme/Font.kt
@@ -0,0 +1,19 @@
+package com.example.jastipin.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.jastipin.R
+
+// Inter Font
+    var interRegular = FontFamily(Font(resId = R.font.interregular))
+    var interMedium = FontFamily(Font(resId = R.font.intermedium))
+    var interBlack = FontFamily(Font(resId = R.font.interblack))
+    var interSemiBold = FontFamily(Font(resId = R.font.intersemibold))
+    var interBold = FontFamily(Font(resId = R.font.interbold))
+    var interExtraBold = FontFamily(Font(resId = R.font.interextrabold))
+
+    // Nunito Font
+    var nunitoSemiBold = FontFamily(Font(resId = R.font.nunitosemibold))
+    var nunitoBold = FontFamily(Font(resId = R.font.nunitobold))
+    var nunitoRegular = FontFamily(Font(resId = R.font.nunitoregular))
+    var nunitoMedium = FontFamily(Font(resId = R.font.nunitomedium))
